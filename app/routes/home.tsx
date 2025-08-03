9f14c87a283f21ed9fc9ca74cbc906d4817e7b8d
doctor-uz <dilshodrahmatov@gmail.com>
2025-08-03 23:44:48 +0200

    putler setup is done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,6 +15,7 @@ export default function Home() {
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
+
       <section className="main-section">
         <div className="page-heading py-16">
           <h1>Track You Applications & Resume Ratings</h1>
