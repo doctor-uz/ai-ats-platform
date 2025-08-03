b5e46f09c4d911a8118ea0ea8e5f01be61273bb6
doctor-uz <dilshodrahmatov@gmail.com>
2025-08-03 23:22:27 +0200

    images fixed, resumeCard and Score added

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,7 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 
 import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -21,11 +22,13 @@ export default function Home() {
         </div>
       </section>
 
-      {resumes.map((resume) => (
-        <div>
-          <h1>{resume.jobTitle}</h1>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
         </div>
-      ))}
+      )}
     </main>
   );
 }
